14ad5877eef68d9a6bd5882a6e183bb6ee501950
Rahul <rahulmishra4317@gmail.com>
2024-02-04 23:56:44 +0530

    commit

--- backend/controller/controller.js
+++ backend/controller/controller.js
@@ -1,35 +1,56 @@
-const {
-    createTodo,
-    updateTodo,
-  } = require("../types");
+const { createTodo, updateTodo } = require("../types");
 
+const todoModel = require("../model/todoModel");
+module.exports.getAllTodo = async (req, res) => {
+  const allTodo = await todoModel.find({})
+};
 
-module.exports.getAllTodo = (req,res) => {
-    
-}
+module.exports.createTodo = async (req, res) => {
+  const createPayload = req.body;
+  const parsePayload = createTodo.safeParse(createPayload);
+  if (!parsePayload.success) {
+    res.status(411).json({
+      msg: "you set the wrong inputs",
+    });
+    return;
+  }
 
-module.exports.createTodo = async (req,res) => {
-    const createPayload = req.body
-    const parsePayload = createTodo.safeParse(createPayload)
-    if(!parsePayload.success) {
-      res.status(411).json({
-        msg  : "you set the wrong inputs"
-      })
-      return;
+  await todoModel.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completed: false,
+  });
+
+  return res.status(201).json({
+    status: true,
+    message: "Todo Created successfully",
+  });
+};
+
+module.exports.completed = async (req, res) => {
+  const updatedPayload = req.body;
+  const parsedPayload = updateTodo.safeParse(updatedPayload);
+
+  if (!parsedPayload.success) {
+    res.status(403).json({
+      status: false,
+      message: "You send the wrong inputs",
+    });
+    return;
+  }
+  await todoModel.update(
+    {
+      _id: req.body.id,
+    },
+    {
+      $set: {
+        completed: true,
+      },
     }
+  );
 
-    const {title , description} = createPayload
-
-    const newTodo = {
-      title , 
-      description
-    }
-
-    
-    
-    
-}
-
-module.exports.completed = (req,res) => {
-    
-}
+  return res.status(200).json({
+    status: true,
+    message: "Todo Marked as completed ",
+  });
+};
