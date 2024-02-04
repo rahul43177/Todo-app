0db13d7a0392221f4da1eba9684236bb2ce04a1a
Rahul <rahulmishra4317@gmail.com>
2024-02-05 01:07:24 +0530

    Commits uptodate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,3 +7,6 @@ function App() {
     </div>
   )
 }
+
+
+export default App
--- frontend/src/Components/CreateTodo.jsx
+++ frontend/src/Components/CreateTodo.jsx
@@ -1,10 +1,33 @@
+/**
+ * Component for creating a new todo
+ * @returns {JSX.Element} - The rendered JSX element
+ */
 export function CreateTodo() {
     return (
         <div>
-            <input type="text" placeholder="Enter Title"/>
-            <br />
-            <input type="text" placeholder="Enter the Description"/>
-            <br />
+            {/* Input field for todo title */}
+            <input 
+                style={{
+                    padding: 10, 
+                    margin: 10, 
+                    border: "2px solid black"
+                }}
+                type="text" 
+                placeholder="Enter Title"
+            />
+            <br /> <br />
+            {/* Input field for todo description */}
+            <input 
+                style={{
+                    padding: 10, 
+                    margin: 10, 
+                    border: "2px solid black"
+                }} 
+                type="text" 
+                placeholder="Enter the Description"
+            />
+            <br /> <br />
+            {/* Button to create the todo */}
             <button>Create Todo</button>
         </div>
     )
